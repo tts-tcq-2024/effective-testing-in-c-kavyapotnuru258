3a7894c314f0734a574952fca2319a316e807707
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-08-26 21:47:17 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -13,23 +13,27 @@ char size(int cms) {
     return sizeName;
 }
 
+#include <stdio.h>
+#include <assert.h>
+
+char size(int cms) {
+    char sizeName = '\0';
+    if(cms < 38) {
+        sizeName = 'S';
+    } else if(cms > 38 && cms < 42) {
+        sizeName = 'M';
+    } else if(cms > 42) {
+        sizeName = 'L';
+    }
+    return sizeName;
+}
+
 int main() {
-    printf("Size for 37 cm: %c\n", size(37)); // Expected output: S
     assert(size(37) == 'S');
-
-    printf("Size for 38 cm: %c\n", size(38)); // Expected output: M
-    assert(size(38) == 'M');
-
-    printf("Size for 40 cm: %c\n", size(40)); // Expected output: M
     assert(size(40) == 'M');
-
-    printf("Size for 42 cm: %c\n", size(42)); // Expected output: L
-    assert(size(42) == 'L');
-
-    printf("Size for 43 cm: %c\n", size(43)); // Expected output: L
     assert(size(43) == 'L');
-
+    assert(size(42) == 'M');
+    assert(size(38) == 'S');
     printf("All is well (maybe!)\n");
     return 0;
 }
-
